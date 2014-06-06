85df182e0e5b7fe6c4478677650713e903059fb3
a-hidetos <hideto_saito@trendmicro.com>
2014-06-07 07:33:26 +0800

    add PWEB-1

---
+++ cookbooks/PWEB-1/django.rb
@@ -0,0 +1,42 @@
+#
+# Cookbook Name:: django
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+remote_file "#{Chef::Config['file_cache_path']}/epel-release-6-8.noarch.rpm" do
+  source "http://mirror-fpt-telecom.fpt.net/fedora/epel/6/i386/epel-release-6-8.noarch.rpm"
+  action :create_if_missing
+end
+
+
+package 'epel-release-repo' do
+  source "#{Chef::Config['file_cache_path']}/epel-release-6-8.noarch.rpm"
+  provider Chef::Provider::Package::Rpm
+  action :install
+end
+
+
+%w{python-pip mod_wsgi httpd}.each do |p|
+        package p do
+                action :install
+        end
+end
+
+
+bash "pip" do
+  user "root"
+  cwd "/tmp"
+  code "/usr/bin/pip install django"
+  action :run
+end
+
+
+service "httpd" do
+  supports :status => true, :start => true, :stop => true, :restart => true
+  action [ :enable, :start ]
+end
+
